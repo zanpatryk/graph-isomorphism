fc2d592f0a3e4e49d352160481bc1fe0007dc3cb
7blak <theblack2233@gmail.com>
2025-11-27 00:44:52 +0100

    added minimal_extension_approximation algorithm

--- algorithms.h
+++ algorithms.h
@@ -2,5 +2,6 @@
 #define AAC_ALGORITHMS_H
 
 #include "algorithms/minimal_extension.h"
+#include "algorithms/minimal_extension_approximation.h"
 
 #endif //AAC_ALGORITHMS_H
---
+++ algorithms/minimal_extension_approximation.h
@@ -0,0 +1,6 @@
+#ifndef AAC_MINIMAL_EXTENSION_APPROXIMATION_H
+#define AAC_MINIMAL_EXTENSION_APPROXIMATION_H
+
+void solve_approximate_extension(int n_g, const int *flat_adj_g, int n_h, const int *flat_adj_h);
+
+#endif //AAC_MINIMAL_EXTENSION_APPROXIMATION_H
